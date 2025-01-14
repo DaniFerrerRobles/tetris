055f9f137fda8d6893a6feec4501dc9f4182e870
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-01-14 20:30:15 +0100

    resolucion conflicto

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './tablaPartidas'
-import TablaPartidas from './tablaPartidas'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from 'react';
+import './App.css';
+import './tablaPartidas';
+import Instrucciones from "./inicio";
+import Juego from "./Juego";
+import Partidas from "./Partidas";
+import Ranking from "./Ranking";
+import TablaPartidas from './tablaPartidas'; // Cambios de origin/main
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <Router>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item"><Link className="nav-link" to="/">Inicio</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/Juego">Juego</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/Partidas">Partidas</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/Ranking">Ranking</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div className="container mt-4">
+      <Routes>
+        <Route path="/" element={<Instrucciones />} />
+        <Route path="/Juego" element={<Juego />} />
+        <Route path="/Partidas" element={<Partidas />} />
+        <Route path="/Ranking" element={<Ranking />} />
+      </Routes>
+    </div>
+    <TablaPartidas />
+  </Router>
+);
 
-  return (
-    <>    
-    <TablaPartidas/>
-    </>
-  )
-}
-
-export default App
+export default App;
