804b431abd9e7802d2870903cb003df68e870037
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-02-03 15:50:57 +0000

    tarea4

--- src/lib/colores.js
+++ src/lib/colores.js
@@ -1,16 +1,16 @@
 export function colorPieza(numero) {
-    const colores = [
-        "bg-white",
-        "bg-secondary",
-        "bg-primary",
-        "bg-black",
-        "bg-success",
-        "bg-danger",
-        "bg-warning",
-        "bg-info",
-        "bg-light",
-        "bg-dark"
-      ]
+  const colores = [
+      "bg-white",
+      "bg-secondary",
+      "bg-primary",
+      "bg-black",
+      "bg-success",
+      "bg-danger",
+      "bg-warning",
+      "bg-info",
+      "bg-light",
+      "bg-dark"
+    ]
 
-      return(colores[numero])
+    return(colores[numero])
 }
