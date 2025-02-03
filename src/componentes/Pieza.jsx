b8a289a226d29b0ab02110dff3a68f51b6d8b674
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-02-03 15:49:46 +0000

    tarea3

--- src/componentes/Pieza.jsx
+++ src/componentes/Pieza.jsx
@@ -6,9 +6,11 @@ export const Pieza = ({ pieza }) => {
     <div>
       {pieza.map((fila, idFila) => (
         <div key={idFila} className="d-flex">
-          {fila.map((casilla, idCasilla) => (
-            <div key={idCasilla} className={colorPieza(casilla)}></div>
-          ))}
+          {fila.map((casilla, idCasilla) => {
+            return (
+              <div key={idCasilla} className={colorPieza(casilla)} style={{border: "1px solid black", width: "20px", height: "20px"}}>{casilla}</div>
+            );
+          })}
         </div>
       ))}
     </div>
